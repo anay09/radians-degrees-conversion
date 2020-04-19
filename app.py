6daede259e888a2cba86fba44e91f38536195522
anayc <anay.chopade>
2020-04-19 17:26:55 +0100

    Added title and input conditions (i.e. if the input is A then do this, etc. Added while loop so that the program can take multiple inputs Imported math library

---
+++ app.py
@@ -0,0 +1,9 @@
+import math
+
+option = ''
+print('------------Radian and Degrees Conversion------------')
+print('Choose one of the following: ')
+print('    - A - Convert from Radians to Degrees')
+print('    - B - Convert from Degrees to Radians')
+print('    - X - Exit\n\n')
+while option == input("Choose the function : "):
